fbceb785c5c2d4500b2647f965c1d1552fca4396
DanNikita <150963002+DanNikita@users.noreply.github.com>
2023-11-16 23:21:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+    public class Calculations
+    {
+        public double Calc(double p)
+        {
+            double Side = p / 3;
+            double s = Math.Sqrt( 0.5 * p * Math.Pow((0.5 * p - Side), 3));
+            return s;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double TrPer = default(double);
+            Console.WriteLine("Enter triangle perimeter:");
+            string temp = Console.ReadLine();
+            TrPer = Convert.ToDouble(temp);
+            var a = new Calculations();
+            double SqResult = a.Calc(TrPer);
+            double SideResult = TrPer/3;
+            Console.WriteLine("{0:F2}", "Side | Square \n" +
+            SideResult + "|" + SqResult);
+            //Console.WriteLine("{0:F2}", SideResult);
+            //Console.WriteLine("{0:F2}", SqResult);
+            Console.ReadLine();
+        }
+    }
+}
